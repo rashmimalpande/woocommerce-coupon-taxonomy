ff1c8d7a34dc38e1a71f294d5d7cd32ce109e80d
Rashmi Malpande <rashmimalpande@gmail.com>
2017-06-28 14:29:10 +0530

    register basic category taxonomy

--- woocommerce-coupon-taxonomy.php
+++ woocommerce-coupon-taxonomy.php
@@ -8,8 +8,33 @@ Author: Rashmi Malpande
 */
 
 
+function wct_create_category(){
+    
+    $labels = array(
+        'name' => 'Coupon Categories',
+        'singular_name' => 'Coupon Category',
+        'search_items' => __('Search Coupon Category'),
+        'all_items' => __('All Categories'),
+        'parent_item' => __('Parent Coupon Category'),
+        'parent_item_colon' => __('Parent Coupon Category:'),
+        'edit_item' => __('Edit Coupon Category'),
+        'update_item' => __('Update Coupon Category'),
+        'add_new_item' => __('Add New Coupon Categry'),
+        'new_item_name' => __('New Coupon Category Name'),
+        'menu_name' => __('Categories'),
 
+    );
 
+    register_taxonomy( 'coupon categories', array('shop_coupon'), array(
+        'hierarchical' => true,
+        'labels' => $labels,
+        'show_ui' => true,
+        'show_admin_column' => true,
+        'query_var' => true,
+        'rewrite' => array( 'slug' => 'coupon_category' ),
+    ) );
+}
+add_action( 'init', 'wct_create_category');
 
 
 
